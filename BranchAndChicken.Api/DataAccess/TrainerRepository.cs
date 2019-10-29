f846c8039e59d32b602813ecbeac3e7d45d6c18a
Nathan Gonzalez <ncgonz@gmail.com>
2019-10-28 20:51:07 -0500

    make it work

--- BranchAndChicken.Api/DataAccess/TrainerRepository.cs
+++ BranchAndChicken.Api/DataAccess/TrainerRepository.cs
@@ -9,77 +9,60 @@ namespace BranchAndChicken.Api.DataAccess
 {
     public class TrainerRepository
     {
-        //static List<Trainer> _trainers = new List<Trainer>
-        //{
-        //    new Trainer
-        //    {
-        //        Name = "Nathan",
-        //        Specialty = Specialty.TaeCluckDoe,
-        //        YearsOfExperience = 0
-        //    },
-        //    new Trainer
-        //    {
-        //        Name = "Martin",
-        //        Specialty = Specialty.Chudo,
-        //        YearsOfExperience = 12
-        //    },
-        //    new Trainer
-        //    {
-        //        Name = "Adam",
-        //        Specialty = Specialty.ChravBacaw,
-        //        YearsOfExperience = 3
-        //    }
-        //};
-
         string _connectionString = "Server=localhost;Database=BranchAndChicken;Trusted_Connection=True;";
 
         public List<Trainer> GetAll()
         {
-            var connection = new SqlConnection(_connectionString);
-            connection.Open();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
 
-            var cmd = connection.CreateCommand();
-            cmd.CommandText = @"Select * 
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"Select * 
                                 From Trainer";
 
-            var dataReader = cmd.ExecuteReader();
+                var dataReader = cmd.ExecuteReader();
 
-            while (dataReader.Read())
-            {
-                //explicit cast
-                var id = (int) dataReader["Id"];
-                //implicit cast
-                var name = dataReader["name"] as string;
-                //convert to
-                var yearsOfExperience = Convert.ToInt32(dataReader["YearsOfExperience"]);
-                //try parse
-                Enum.TryParse<Specialty>(dataReader["speciality"].ToString(), out var speciality);
+                var trainers = new List<Trainer>();
 
-                var trainer = new Trainer
+                while (dataReader.Read())
                 {
-                    Specialty = speciality,
-                    Id = id,
-                    Name = name,
-                    YearsOfExperience = yearsOfExperience
-                };
+                    //explicit cast
+                    var id = (int) dataReader["Id"];
+                    //implicit cast
+                    var name = dataReader["name"] as string;
+                    //convert to
+                    var yearsOfExperience = Convert.ToInt32(dataReader["YearsOfExperience"]);
+                    //try parse
+                    Enum.TryParse<Specialty>(dataReader["speciality"].ToString(), out var speciality);
 
+                    var trainer = new Trainer
+                    {
+                        Specialty = speciality,
+                        Id = id,
+                        Name = name,
+                        YearsOfExperience = yearsOfExperience
+                    };
+
+                    trainers.Add(trainer);
+                }
+                return trainers;
             }
-
-
-            return _trainers;
         }
 
         public Trainer Get(string name)
         {
-            var trainer = _trainers.First(t => t.Name == name);
-            return trainer;
+            //var trainer = _trainers.First(t => t.Name == name);
+            //return trainer;
+            return new Trainer();
         }
 
         public void Remove(string name)
         {
-            var trainer = Get(name);
+            //var trainer = Get(name);
 
-            _trainers.Remove(trainer);
+            //_trainers.Remove(trainer);
+
         }
 
         public ActionResult<Trainer> GetSpecialty(string specialty)
@@ -89,16 +72,17 @@ namespace BranchAndChicken.Api.DataAccess
 
         public Trainer Update(Trainer updatedTrainer, int id)
         {
-            var trainerToUpdate = _trainers.First(trainer => trainer.Id == id);
-            trainerToUpdate.Name = updatedTrainer.Name;
-            trainerToUpdate.YearsOfExperience = updatedTrainer.YearsOfExperience;
-            trainerToUpdate.Specialty = updatedTrainer.Specialty;
-            return trainerToUpdate;
+            //var trainerToUpdate = _trainers.First(trainer => trainer.Id == id);
+            //trainerToUpdate.Name = updatedTrainer.Name;
+            //trainerToUpdate.YearsOfExperience = updatedTrainer.YearsOfExperience;
+            //trainerToUpdate.Specialty = updatedTrainer.Specialty;
+            //return trainerToUpdate;
+            return updatedTrainer;
         }
 
         public Trainer Add(Trainer newTrainer)
         {
-            _trainers.Add(newTrainer);
+            //_trainers.Add(newTrainer);
             return newTrainer;
         }
     }
